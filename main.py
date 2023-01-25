4bd41207cc31fa0f63f576d04cc352c219c939ef
0HungryDeveloper <hungrystudentdev@gmail.com>
2023-01-25 13:05:17 -0600

    Log File implementation

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
 from time import time
+import logging
 
+enrollment = 'al02841065.log'
+logging.basicConfig(filename=enrollment, encoding='utf-8', level=logging.INFO)
 start_time = time()
 
 def open_file(file_name: str):
     start = time()
-    directory = "D:\Descargas\CS13309_Archivos_HTML" 
-    open(directory + "/Files/" + file_name + ".html", "r")
+    # !Windows version
+    # directory = "D:\Descargas\CS13309_Archivos_HTML"
+    # open(directory + "/Files/" + file_name + ".html", "r")
+    # !IOS version
+    open("./Files/" + file_name + ".html", "r")
     finish = time()
     execution_time = finish - start
-    return "El archivo " + file_name + ".html tardo " + "{:.4f}".format(execution_time) + " en abrir."
+    logging.info("El archivo " + file_name + ".html tardo " + "{:.4f}".format(execution_time) + " en abrir.")
 
 for i in range(2,504):
     if (i < 10):
@@ -20,4 +26,4 @@ for i in range(2,504):
 
 final_time = time()
 elapsed_time = final_time - start_time
-print("El proceso tardó en ejecutarse " + "{:.2f}".format(elapsed_time) + " segundos")
+logging.info("El proceso tardó en ejecutarse " + "{:.2f}".format(elapsed_time) + " segundos")
