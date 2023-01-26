03b4415277827d0c71233a81977338b64b3fe4d7
DestroyerRexor <edgardejesus_01@hotmail.com>
2023-01-25 18:37:47 -0600

    suma de procesos

--- main.py
+++ main.py
@@ -4,17 +4,21 @@ import logging
 enrollment = 'al02841065.log'
 logging.basicConfig(filename=enrollment, encoding='utf-8', level=logging.INFO)
 start_time = time()
+sum_total = 0
 
 def open_file(file_name: str):
     start = time()
     # !Windows version
-    # directory = "D:\Descargas\CS13309_Archivos_HTML"
-    # open(directory + "/Files/" + file_name + ".html", "r")
+    directory = "D:\Descargas\CS13309_Archivos_HTML"
+    open(directory + "/Files/" + file_name + ".html", "r")
     # !IOS version
-    open("./Files/" + file_name + ".html", "r")
+    #open("./Files/" + file_name + ".html", "r")
     finish = time()
     execution_time = finish - start
     logging.info("El archivo " + file_name + ".html tardo " + "{:.4f}".format(execution_time) + " en abrir.")
+    print("El archivo " + file_name + ".html tardo " + "{:.4f}".format(execution_time) + " en abrir.")
+    global sum_total
+    sum_total += execution_time
 
 for i in range(2,504):
     if (i < 10):
@@ -26,4 +30,8 @@ for i in range(2,504):
 
 final_time = time()
 elapsed_time = final_time - start_time
+
+logging.info("El tiempo total de los procesos fue de  " + "{:.4f}".format(sum_total) + " segundos")
+print("El tiempo total de los procesos fue de  " + "{:.4f}".format(sum_total) + " segundos")
 logging.info("El proceso tardó en ejecutarse " + "{:.2f}".format(elapsed_time) + " segundos")
+print("El proceso tardó en ejecutarse " + "{:.2f}".format(elapsed_time) + " segundos")
