8fb2212c30c9327d0ca1b4d4219316593a6ecf79
LAPTOP-RVI1O231\alexm <alexmalcom.03gmail.com>
2023-03-03 18:33:44 -0600

    tokenization

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import logging
 import re
 import os
 from bs4 import BeautifulSoup
+from collections import Counter
 
 # ! Importar BeatifulSoup desde la terminal con el siguiente comando ---> pip install beautifulsoup4
 
@@ -20,6 +21,9 @@ sum_total = 0
 
 file_words = []
 
+# diccionario para almacenar el recuento de palabras en todos los archivos
+word_count_all = {}
+
 """
     Abrir archivos HTML.
     @param file_name: Nombre del archivo HTML.
@@ -28,10 +32,11 @@ file_words = []
 
 def open_file(file_name: str):
 
+
     # ! Se comienza a correr el tiempo de ejecución de la función.
     start = time()
     
-    file_path = os.path.join(os.getcwd(),'Files', file_name + '.html')
+    file_path = os.path.join(os.getcwd(),'CS13309_Archivos_HTML','Files', file_name + '.html')
 
     # Abrir y leer el archivo, ignoramos cualquier posible error y asignamos un alías.
     with open(file_path, "r", errors='ignore') as fileR:
@@ -41,12 +46,11 @@ def open_file(file_name: str):
     # Se eliminan las etiquetas HTML.
     text_without_tags = remove_html_tags(text_html)
 
-    # Buscamos y ordenamos las palabras aceptadas
-    accepted_words = search_words_and_sort(text_without_tags)
-
-    #Escribimos el resultado en los archivos
-    with open(file_path, "w", errors='ignore') as fileW:
-        fileW.write(str(accepted_words))
+# Cuenta las palabras del texto y guardarlas en un diccionario.
+    count_words_save(text_without_tags)
+    global  word_count_all 
+    word_count_all = dict(sorted(word_count_all.items()))
+    
 
     # ! Fin de ejecución de la función.
     finish = time()
@@ -89,6 +93,27 @@ def search_words_and_sort(text: str):
             #//if not found_word in file_words: # Condicion para eliminar palabras duplicadas, aunque esto no es lo que pide la actividad, pero puede servir
             file_words.append(found_word.split()[0])
 
+#funcion para contar las palabras del documento y agregarlas al archivo.
+def count_words_save(text):
+
+    # Buscamos y ordenamos las palabras aceptadas
+    tokens = find_words(text)
+
+    # Contamos cuántas veces aparece cada palabra
+    word_count = Counter(tokens)
+
+    # agregar el recuento de palabras al diccionario global
+    for word, count in word_count.items():
+        if word in word_count_all:
+            word_count_all[word.lower()] += count
+        else:
+            word_count_all[word.lower()] = count
+    
+    
+    # guardar los resultados en un archivo de texto
+    with open("resultados.txt", "w") as output_file:
+        for word, count in word_count_all.items():
+            output_file.write(f"{word}\t{count}\n")
 
 # Se recorre la lista de arhcivos.
 for i in range(2, 504):
@@ -107,7 +132,7 @@ print(f"El tiempo total de los procesos fue de  {sum_total:.4f} segundos")
 logging.info(f"El proceso tardó en ejecutarse {elapsed_time:.2f} segundos")
 print(f"El proceso tardó en ejecutarse {elapsed_time:.2f} segundos")
 # print(f"Lista de todas las palabras ordenadas: {str(file_words)}")
-accepted_file_words_path = os.path.join(os.getcwd(), 'Lista.txt')
-with open(accepted_file_words_path, "w") as f:
-    for word in file_words:
-        f.write(word + '\n')
+# accepted_file_words_path = os.path.join(os.getcwd(), 'Lista.txt')
+#with open(accepted_file_words_path, "w") as f:
+#    for word in file_words:
+#       f.write(word + '\n')
