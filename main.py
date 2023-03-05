0ffc5fb7823b0cd51ac0df512975c4e08d404646
0HungryDeveloper <hungrystudentdev@gmail.com>
2023-03-04 20:24:50 -0600

    Count word on files

--- main.py
+++ main.py
@@ -2,10 +2,10 @@ from time import time
 import logging
 import re
 import os
+# ! Importar BeatifulSoup desde la terminal con el siguiente comando ---> pip install beautifulsoup4
 from bs4 import BeautifulSoup
 from collections import Counter
 
-# ! Importar BeatifulSoup desde la terminal con el siguiente comando ---> pip install beautifulsoup4
 
 # * Crear el archivo log.
 enrollment = 'PYTHONLOVERS.log'
@@ -24,15 +24,8 @@ file_words = []
 # diccionario para almacenar el recuento de palabras en todos los archivos
 word_count_all = {}
 
-"""
-    Abrir archivos HTML.
-    @param file_name: Nombre del archivo HTML.
-"""
-
-
+# * Abrir archivos HTML.
 def open_file(file_name: str):
-
-
     # ! Se comienza a correr el tiempo de ejecución de la función.
     start = time()
 
@@ -46,11 +39,10 @@ def open_file(file_name: str):
     # Se eliminan las etiquetas HTML.
     text_without_tags = remove_html_tags(text_html)
 
-# Cuenta las palabras del texto y guardarlas en un diccionario.
+    # Cuenta las palabras del texto y guardarlas en un diccionario.
     count_words_save(text_without_tags)
-    global  word_count_all 
+    global  word_count_all
     word_count_all = dict(sorted(word_count_all.items()))
-    
 
     # ! Fin de ejecución de la función.
     finish = time()
@@ -65,13 +57,6 @@ def open_file(file_name: str):
     global sum_total
     sum_total += execution_time
 
-
-"""
-    Remover etiquetas HTML.
-    @param text: Archivo HTML convertido a string.
-    @return Texto sin etiquetas HTML.
-"""
-
 # * Funcion para remover todos los tags de html usando BeatifulSoup
 def remove_html_tags(text):
     soup = BeautifulSoup(text, "html.parser")
@@ -84,16 +69,7 @@ def find_words(text):
     words = re.findall(patron, text)
     return words
 
-# * Funcion para buscar y ordenar cada palabra de un texto
-def search_words_and_sort(text: str):
-    global file_words
-    for found_word in find_words(text.lower()):
-        if found_word:
-            # Si se encuentra una palabra de la funcion find_words la agregamos a la lista cada palabra
-            #//if not found_word in file_words: # Condicion para eliminar palabras duplicadas, aunque esto no es lo que pide la actividad, pero puede servir
-            file_words.append(found_word.split()[0])
-
-#funcion para contar las palabras del documento y agregarlas al archivo.
+# * Funcion para contar las palabras del documento y agregarlas al archivo.
 def count_words_save(text):
 
     # Buscamos y ordenamos las palabras aceptadas
@@ -108,31 +84,58 @@ def count_words_save(text):
             word_count_all[word.lower()] += count
         else:
             word_count_all[word.lower()] = count
-    
-    
+
     # guardar los resultados en un archivo de texto
     with open("resultados.txt", "w") as output_file:
         for word, count in word_count_all.items():
-            output_file.write(f"{word}\t{count}\n")
+            count_file = count_word_on_file(word)
+            output_file.write(f"{word}\t{count}\t{count_file}\n")
 
-# Se recorre la lista de arhcivos.
-for i in range(2, 504):
-    # Se aplica un formato al string para agregar un cero o dos dependiendo del nombre
-    # del archivo.
-    print(open_file("%03d" % i))
+def count_word_on_file(word_to_count):
+    counter = 0
+    files = ['simple.html', 'medium.html', 'hard.html']
 
-# ! Se termina de ejecutar el programa.
-final_time = time()
-elapsed_time = final_time - start_time
-file_words.sort()
+    file_path = os.path.join(os.getcwd(), 'CS13309_Archivos_HTML', 'Files') + os.sep
 
-# * Información del tiempo para crear el archivo log
-logging.info(f"El tiempo total de los procesos fue de {sum_total:.4f} segundos")
-print(f"El tiempo total de los procesos fue de  {sum_total:.4f} segundos")
-logging.info(f"El proceso tardó en ejecutarse {elapsed_time:.2f} segundos")
-print(f"El proceso tardó en ejecutarse {elapsed_time:.2f} segundos")
-# print(f"Lista de todas las palabras ordenadas: {str(file_words)}")
-# accepted_file_words_path = os.path.join(os.getcwd(), 'Lista.txt')
-#with open(accepted_file_words_path, "w") as f:
-#    for word in file_words:
-#       f.write(word + '\n')
+    for file in files:
+        with open(file_path + file, 'r', errors='ignore') as fileR:
+        # Guardamos el resultado en un string para poder leerlo y modificarlo.
+            text_html = fileR.read()
+
+        text = remove_html_tags(text_html)
+        text_lower = " ".join(word.lower() for word in text.split())
+
+        with open(file_path + file, 'w') as file_writer:
+            file_writer.write(text_lower)
+
+    for file in files:
+        with open(file_path + file, 'r') as f:
+            text = f.read()
+            # Buscamos la palabra exacta en el archivo usando una expresión regular
+            matches = re.findall(rf'\b{word_to_count}\b', text)
+            if matches:
+                counter += 1
+
+    return counter
+
+if __name__ == '__main__':
+    # Se recorre la lista de arhcivos.
+    for i in range(2, 504):
+        # Se aplica un formato al string para agregar un cero o dos dependiendo del nombre
+        # del archivo.
+        open_file("%03d" % i)
+
+    open_file('simple')
+    open_file('medium')
+    open_file('hard')
+
+    # ! Se termina de ejecutar el programa.
+    final_time = time()
+    elapsed_time = final_time - start_time
+    file_words.sort()
+
+    # * Información del tiempo para crear el archivo log
+    logging.info(f"El tiempo total de los procesos fue de {sum_total:.4f} segundos")
+    print(f"El tiempo total de los procesos fue de  {sum_total:.4f} segundos")
+    logging.info(f"El proceso tardó en ejecutarse {elapsed_time:.2f} segundos")
+    print(f"El proceso tardó en ejecutarse {elapsed_time:.2f} segundos")
